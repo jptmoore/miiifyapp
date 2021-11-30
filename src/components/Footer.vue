63539b38ef9a91271e26baa7cf5dac7551f1c28a
John Moore <jmoore@zedstar.org>
2021-11-30 13:22:14 +0000

    adding missing files

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer>
+    <p>miiify.rocks</p>
+  </footer>
+</template>
+
+<style scoped>
+a {
+  color: #333;
+}
+
+footer {
+  margin-top: 30px;
+  text-align: center;
+}
+</style>
